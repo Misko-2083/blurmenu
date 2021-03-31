635ee79ef99054213c28ea5b045103859c3bdb82
Johan Malm <jgm323@gmail.com>
2021-03-31 09:24:21 +0100

    Refactor blurmenu.c

--- blurmenu.c
+++ blurmenu.c
@@ -16,38 +16,19 @@
 #include <unistd.h>
 #include "stackblur.h"
 
-/* Select an area to save to "image.png" in the current folder
-   gcc  screenshot.c -Wall -o  screenshot
-   `pkg-config --cflags --libs x11 xrender cairo` -lpthread
-*/
-
 // This is measured to be slightly faster.
-#define GetPixel(ximg, x, y) ((u_int32_t *)&(ximg->data[y * ximg->bytes_per_line]))[x]
+#define GetPixel(_ximg_, _x_, _y_) ((u_int32_t *)&(ximg->data[_y_ * _ximg_->bytes_per_line]))[_x_]
 
-cairo_surface_t *get_root_ximage(int cordx, int cordy, int cordw, int cordh)
+cairo_surface_t *covert_to_cairo(XImage *ximg, int width, int height)
 {
-	Display* dpy = XOpenDisplay(NULL);
-	Screen *scr = NULL;
-	scr = ScreenOfDisplay(dpy, DefaultScreen(dpy));
-	Window root = 0;
-	root = RootWindow(dpy, XScreenNumberOfScreen(scr));
 	cairo_surface_t *surface = NULL;
-	XImage *ximg;
+	int stride = cairo_format_stride_for_width(CAIRO_FORMAT_RGB24, width);
 
-	ximg = XGetImage(dpy, root, cordx, cordy, cordw, cordh, AllPlanes, ZPixmap);
+	unsigned char *data = calloc(1, stride * height);
 
-	stackblur(ximg,0,0,cordw,cordh,10, 2);
-
-	XCloseDisplay(dpy);
-
-	int stride;
-	stride = cairo_format_stride_for_width(CAIRO_FORMAT_RGB24, cordw);
-
-	unsigned char *data = calloc(1, stride * cordh);
-	int x, y;
-	for (y = 0; y < cordh; ++y) {
-		for (x = 0; x < cordw; ++x) {
-			unsigned long pixel = GetPixel(ximg, x, y);
+	for (int y = 0; y < height; ++y) {
+		for (int x = 0; x < width; ++x) {
+			unsigned long pixel = XGetPixel(ximg, x, y);
 			unsigned char blue = (ximg->blue_mask & pixel);
 			unsigned char green = (ximg->green_mask & pixel) >> 8;
 			unsigned char red = (ximg->red_mask & pixel) >> 16;
@@ -58,23 +39,31 @@ cairo_surface_t *get_root_ximage(int cordx, int cordy, int cordw, int cordh)
 	}
 
 	surface = cairo_image_surface_create_for_data(data, CAIRO_FORMAT_RGB24,
-						      cordw, cordh, stride);
+						      width, height, stride);
 	cairo_surface_mark_dirty(surface);
-	if (ximg)
-		XDestroyImage(ximg);
 	return surface;
 }
 
-int main (int argc, char *argv[])
+XImage *get_root_ximage(int x, int y, int w, int h)
 {
-	int cordx, cordy, cordw, cordh;
-	cordx = 10; cordy = 10; cordw = 100; cordh = 100;
-
-	printf("X=%d\nY=%d\nWIDTH=%d\nHEIGHT=%d\n", cordx, cordy, cordw, cordh);
-
-	cairo_surface_t *image_surface = NULL;
-	image_surface = get_root_ximage(cordx, cordy, cordw, cordh);
-	cairo_surface_write_to_png(image_surface, "image.png");
-	cairo_surface_destroy(image_surface);
-	return 0;
+	Display* dpy = XOpenDisplay(NULL);
+	Screen *scr = ScreenOfDisplay(dpy, DefaultScreen(dpy));
+	Window root = RootWindow(dpy, XScreenNumberOfScreen(scr));
+	XImage *ximg = XGetImage(dpy, root, x, y, w, h, AllPlanes, ZPixmap);
+	XCloseDisplay(dpy);
+	return ximg;
 }
+
+int main(int argc, char *argv[])
+{
+	int x=10, y=10, w=100, h=100;
+	int radius = 10;
+	XImage *ximg = get_root_ximage(x, y, w, h);
+	stackblur(ximg, 0, 0, w, h, radius, 2);
+	cairo_surface_t *surf = covert_to_cairo(ximg, w, h);
+	if (ximg)
+		XDestroyImage(ximg);
+	cairo_surface_write_to_png(surf, "image.png");
+	cairo_surface_destroy(surf);
+}
+
